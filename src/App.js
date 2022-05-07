8b826960346d58f40fda8a27be9076db6d77ddc7
kankit08 <kankit08@gmail.com>
2022-05-07 20:55:30 +0530

    timer and queston are updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import moneyPyramid from './dataMoney'
 
 const App = () => {
-  
+  const [questionNumber, setQuestionNumber] = useState(1)
   return (
     <div className='app'>
-        <div className="main">main</div>
+        <div className="main">
+          <div className="top">
+            <div className="timer">30</div>
+          </div>
+          <div className="bottom">Questions & Answers</div>
+        </div>
         <div className="pyramid">
             <ul className="moneylist">
               {moneyPyramid.map((m) => (
-                <li className="moneyListItem">
+                <li className= {questionNumber === m.id ? "moneyListItem active":"moneyListItem"}>
                   <span className='moneyListItemNumber'>{m.id}</span>
                   <span className='moneyListItemAmount'>{m.amount}</span>
                 </li>
